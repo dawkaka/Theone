6fdd2d503bcfffb335fcc77be13dbfb8d85e269f
dawkaka <yousiph77@gmail.com>
2022-12-04 21:18:00 +0000

    fix: Add cors domain to config

--- app/middlewares/cors.go
+++ app/middlewares/cors.go
@@ -1,10 +1,13 @@
 package middlewares
 
-import "github.com/gin-gonic/gin"
+import (
+	config "github.com/dawkaka/theone/conf"
+	"github.com/gin-gonic/gin"
+)
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", config.CORS_DOMAIN)
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
